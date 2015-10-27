42450be5a66116acafce88ac29b07434158c9c71
Daniel Stout <dstout55@gmail.com>
2015-10-27 15:20:20 -0300

    Added basic CharacterControl script

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumpSpeed = 10;
+    public float gravity = 10;
+
+    private CharacterController control;
+
+    void Start()
+    {
+        control = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var xMov = Input.GetAxis("Horizontal");
+        var yMov = Input.GetAxis("Vertical");
+        var isJumping = Input.GetButton("Jump");
+        
+        var vec = new Vector3(xMov, 0, yMov);
+        vec = transform.TransformDirection(vec);
+
+        if (control.isGrounded && isJumping)
+            vec.y = jumpSpeed;
+
+        vec.y -= gravity * Time.deltaTime;
+
+        control.Move(vec * speed * Time.deltaTime);
+    }
+}
