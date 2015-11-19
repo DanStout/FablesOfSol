02422cb43d7a68c5a211d59ebdf259fbf2e0c5a6
Matt <burkem.1993@gmail.com>
2015-11-18 20:25:37 -0400

    Added comments.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CameraControl : MonoBehaviour
 {
     public Transform playerTransform;
-	public float speed;
+	public float speed; //speed that the camera will move at when moving
     private Vector3 initialOffset;
 	private Camera cam;
 
@@ -16,8 +16,12 @@ public class CameraControl : MonoBehaviour
 
     void Update()
     {
-        
+		//viewPos stores the position of the player relative to the viewscreens edges        
 		Vector3 viewPos = cam.WorldToViewportPoint (playerTransform.position);
+
+
+		//If the player is within the specified distance to any edge
+		//move the camera towards the player.
 		if (viewPos.x > 0.7F || viewPos.x < 0.3F || viewPos.y < 0.3F || viewPos.y > 0.7f) {
 			transform.position = Vector3.MoveTowards(transform.position, 
 			                                         playerTransform.position + initialOffset, 
