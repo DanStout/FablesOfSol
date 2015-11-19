e9e9982b160f4ede89baf0951905e71aa3e9884f
Matt <burkem.1993@gmail.com>
2015-11-18 20:08:21 -0400

    Modified camera control script to move when screen boundary is approached.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class CameraControl : MonoBehaviour
 {
     public Transform playerTransform;
+	public float speed;
     private Vector3 initialOffset;
+	private Camera cam;
 
     void Start()
     {
+		cam = GetComponent<Camera>();
         initialOffset = transform.position - playerTransform.position;
     }
 
     void Update()
     {
-        transform.position = playerTransform.position + initialOffset;
-    }
-
+        
+		Vector3 viewPos = cam.WorldToViewportPoint (playerTransform.position);
+		if (viewPos.x > 0.7F || viewPos.x < 0.3F || viewPos.y < 0.3F || viewPos.y > 0.7f) {
+			transform.position = Vector3.MoveTowards(transform.position, 
+			                                         playerTransform.position + initialOffset, 
+			                                         speed * Time.deltaTime);
+		}
+    }	
 }
