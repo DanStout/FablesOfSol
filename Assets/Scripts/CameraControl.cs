3f2759cf646c7adb1894e04eb5226a774fd6c407
Daniel Stout <dstout55@gmail.com>
2015-11-21 14:44:36 -0400

    Uncommented better camera movement

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,31 +3,36 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour
 {
-    public Transform playerTransform;
-    public float speed; //speed that the camera will move at when moving
+    [SerializeField]
+    private GameObject player;
+
+    private Transform playerTransform;
+    private CharacterControl playerControl;
     private Vector3 initialOffset;
     private Camera cam;
+    private float speed;
 
     void Start()
     {
         cam = GetComponent<Camera>();
+        playerTransform = player.transform;
+        playerControl = player.GetComponent<CharacterControl>();
+        speed = playerControl.moveSpeed;
         initialOffset = transform.position - playerTransform.position;
     }
 
     void Update()
     {
-        transform.position = playerTransform.position + initialOffset;
-        ////viewPos stores the position of the player relative to the viewscreens edges        
-        //Vector3 viewPos = cam.WorldToViewportPoint(playerTransform.position);
+        //viewPos stores the position of the player relative to the viewscreens edges        
+        Vector3 viewPos = cam.WorldToViewportPoint(playerTransform.position);
 
-
-        ////If the player is within the specified distance to any edge
-        ////move the camera towards the player.
-        //if (viewPos.x > 0.7F || viewPos.x < 0.3F || viewPos.y < 0.3F || viewPos.y > 0.7f)
-        //{
-        //    transform.position = Vector3.MoveTowards(transform.position,
-        //                                             playerTransform.position + initialOffset,
-        //                                             speed * Time.deltaTime);
-        //}
+        //If the player is within the specified distance to any edge
+        //move the camera towards the player.
+        if (viewPos.x > 0.7F || viewPos.x < 0.3F || viewPos.y < 0.3F || viewPos.y > 0.7f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                                                     playerTransform.position + initialOffset,
+                                                     speed * Time.deltaTime);
+        }
     }
 }
