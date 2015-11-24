1e9cf2b9f93276a8f8de822203bf03ae85a20e31
Daniel Stout <dstout55@gmail.com>
2015-11-24 12:33:19 -0400

    Moved ladder pack, added some more textures, and finished door puzzle in lab

---
+++ Assets/Scripts/PositionToggleable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionToggleable : MonoBehaviour
+{
+    public Vector3 positionAddition;
+    private bool isToggled;
+
+    public void Toggle()
+    {
+        if (isToggled)
+        {
+            transform.localPosition -= positionAddition;
+        }
+        else
+        {
+            transform.localPosition += positionAddition;
+        }
+        isToggled = !isToggled;
+    }
+}
