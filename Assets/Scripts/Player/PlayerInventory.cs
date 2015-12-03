728b46b807e798652769133546c3bbc780eda125
Matt <burkem.1993@gmail.com>
2015-12-03 12:24:46 -0400

    Modified UI for item pickups

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour
 {
     //Variables to store current items and active items
     private IItem activeItem;
     public List<IItem> inventory;
+	private GameObject UI;
 
     void Start()
     {
+		//Find inventory UI
+		UI = GameObject.FindGameObjectWithTag ("InventoryUI");
+
         //Initialize inventory with hammer
         inventory = new List<IItem>();
         Hammer hammer = this.gameObject.AddComponent<Hammer>();
@@ -32,9 +37,38 @@ public class PlayerInventory : MonoBehaviour
         //If we have collided with an item, add to inventory
         if (hit.gameObject.tag == "item")
         {
-            IItem newItem = (IItem)hit.gameObject.GetComponent(typeof(IItem));
-            pickup(newItem);
-            Destroy(hit.gameObject);
+			//Add item script to inventory
+			IItem newItem = (IItem)hit.gameObject.GetComponent(typeof(IItem));
+			pickup(newItem);
+			Destroy(hit.gameObject);
+
+			//Create inventory UI element
+			try
+			{
+				GameObject button = (GameObject)Instantiate(Resources.Load("ButtonPrefab"));
+				Button b = (Button) button.GetComponent(typeof(Button));
+				if(b != null)
+				{
+					b.onClick.AddListener(() => {
+						print ("setActive");
+						setActive((IItem)b.GetComponent(typeof(IItem)));
+					});
+				}
+
+				Text text = (Text)button.GetComponentInChildren(typeof(Text));
+				text.text = "";
+		
+
+				button.AddComponent(newItem.GetType());
+				button.transform.SetParent(UI.transform);
+			}
+			catch(UnityException e)
+			{
+				print (e.GetBaseException());
+			}
+
+
+
         }
     }
 
@@ -54,6 +88,11 @@ public class PlayerInventory : MonoBehaviour
             activeItem = item;
         }
     }
+
+	public void setActive(IItem item)
+	{
+		activeItem = item;
+	}
 
     public void PickupItem(BaseItem item)
     {
