c99137ec707da89d10cd6f86950371469c641dfa
Daniel Stout <dstout55@gmail.com>
2015-12-03 11:27:19 -0400

    Added basic Cronon teleporter and destroyable rock on Bau

---
+++ Assets/Scripts/Environment/Destroyable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyable : MonoBehaviour
+{
+    public int hitsRequired = 2;
+    private int hitsLeft;
+
+    void Start()
+    {
+        hitsLeft = hitsRequired;
+    }
+
+    public void TakeHit()
+    {
+        hitsLeft--;
+        if (hitsLeft == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+
+}
---
+++ Assets/Scripts/Operables/Teleporter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : BaseOperable
+{
+    public override void Operate()
+    {
+        Application.LoadLevel("2 - Cronon");
+    }
+}
