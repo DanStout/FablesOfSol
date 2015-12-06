deeee9ad27f3d471aa93ecf5cceeef4d0c662594
Daniel Stout <dstout55@gmail.com>
2015-12-06 11:45:58 -0400

    Added point lights to dropped items and fixed height offset bug. Also made Thrum random movement more complex

--- Assets/Scripts/Enemies/Behaviours/MovesRandomly.cs
+++ Assets/Scripts/Enemies/Behaviours/MovesRandomly.cs
@@ -6,24 +6,29 @@ public class MovesRandomly : MonoBehaviour
 {
     public float moveRadius = 10;
     public float arrivedDistance = 1f;
+    private float distToDest;
+    public float secondsToStayAtDestination = 1;
+    public float secondsToCheckForSameDestination = 10;
 
+    private bool isChoosing = false;
     private NavMeshAgent agent;
+    private Vector3 lastPosition;
 
 	void Start()
 	{
         agent = GetComponent<NavMeshAgent>();
         agent.destination = transform.position;
+        StartCoroutine(DestinationTakingTooLong());
 	}
 
 	void Update()
 	{
         Debug.DrawLine(transform.position, agent.destination);
 
-        var distToDest = Vector3.Distance(transform.position, agent.destination);
-	    if (distToDest<= arrivedDistance)
+        distToDest = Vector3.Distance(transform.position, agent.destination);
+	    if (!isChoosing && distToDest <= arrivedDistance)
         {
-            Wait(2);
-            MoveToNextPoint();
+            StartCoroutine(WaitBeforeMoving(secondsToStayAtDestination));
         }
 	}
 
@@ -37,13 +42,30 @@ public class MovesRandomly : MonoBehaviour
             agent.SetDestination(hit.position);
         }
     }
-    private IEnumerator Wait(float seconds)
+
+    private IEnumerator WaitBeforeMoving(float seconds)
     {
+        isChoosing = true;
         yield return new WaitForSeconds(seconds);
+        MoveToNextPoint();
+        isChoosing = false;
     }
 
     public void Die()
     {
         Destroy(this);
+    }
+
+    private IEnumerator DestinationTakingTooLong()
+    {
+        while(true)
+        {
+            var oldDest = agent.destination;
+            yield return new WaitForSeconds(secondsToCheckForSameDestination);
+            var newDest = agent.destination;
+
+            if (oldDest == newDest)
+                StartCoroutine(WaitBeforeMoving(0));
+        }
     }
 }
