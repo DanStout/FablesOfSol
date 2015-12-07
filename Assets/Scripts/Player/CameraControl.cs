a216a496873762946c0d8f932d41fe0064bed76e
DandiAndy <a9kenny@gmail.com>
2015-12-07 16:52:59 -0400

    More Floe Doe

--- Assets/Scripts/Player/CameraControl.cs
+++ Assets/Scripts/Player/CameraControl.cs
@@ -19,23 +19,19 @@ public class CameraControl : MonoBehaviour
 
     void Update()
     {
-		if (playerTransform.parent.tag != "IceSurface") {
-			//viewPos stores the position of the player relative to the viewscreens edges        
-			Vector3 viewPos = Camera.main.WorldToViewportPoint (playerTransform.position);
+        //viewPos stores the position of the player relative to the viewscreens edges        
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(playerTransform.position);
 
-			//If the player is within the specified distance to any edge
-			//move the camera towards the player.
-			if (viewPos.x > 0.7F || viewPos.x < 0.3F || viewPos.y < 0.3F || viewPos.y > 0.7f) {
-				var current = transform.position;
-				var target = playerTransform.position + initialOffset;
-				var increment = speed * Time.deltaTime;
+        //If the player is within the specified distance to any edge
+        //move the camera towards the player.
+        if (viewPos.x > 0.7F || viewPos.x < 0.3F || viewPos.y < 0.3F || viewPos.y > 0.7f)
+        {
+            var current = transform.position;
+            var target = playerTransform.position + initialOffset;
+            var increment = speed * Time.deltaTime;
 
-				transform.position = Vector3.MoveTowards (current, target, increment);
-			}
-		} else {
-			Vector3 iceViewPos = Camera.main.WorldToViewportPoint(playerTransform.parent.transform.position);
-		}
-
+            transform.position = Vector3.MoveTowards(current, target, increment);
+        }
     }
 
     public void RecenterOnPlayer()
