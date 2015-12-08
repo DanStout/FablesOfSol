939a0812a3135cb043b5a99999aee2f5a72aa72b
Matt <burkem.1993@gmail.com>
2015-12-07 21:06:56 -0400

    Resolving conflicts in magnet gun

--- Assets/Scripts/Items/MagnetGun.cs
+++ Assets/Scripts/Items/MagnetGun.cs
@@ -1,18 +1,16 @@
 using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
-public class MagnetGun : BaseItem {
-=======
+
 public class MagnetGun : BaseItem
 {
     private GameObject owner;
     private bool isOn;
     private ParticleSystem particleSys;
->>>>>>> 5c874fbcd44fe2e8a0e88c9c8658823f3cc96609
+
 
     // Use this for initialization
-    void Start()
+    override void Awake()
     {
         owner = GameObject.FindGameObjectWithTag("Player");
         isOn = false;
@@ -24,31 +22,6 @@ public class MagnetGun : BaseItem
                 particleSys = p;
         }
     }
-
-<<<<<<< HEAD
-	// Use this for initialization
-	override void Start(){
-=======
-    // Update is called once per frame
-    void Update()
-    {
-        //Check if the gun is on
-        if (isOn)
-        {
-            RaycastHit hit;
-            Vector3 fwd = owner.transform.TransformDirection(Vector3.forward);
-            Vector3 pos = new Vector3(0, 1, 0);
->>>>>>> 5c874fbcd44fe2e8a0e88c9c8658823f3cc96609
-
-
-<<<<<<< HEAD
-		ParticleSystem[] systems = owner.GetComponentsInChildren<ParticleSystem>();
-		foreach(ParticleSystem p in systems)
-		{
-			if(p.gameObject.name == "MagnetGunParticle")
-				particleSys = p;
-		}
-	}
 	
 	// Update is called once per frame
 	override void Update () {
@@ -59,27 +32,7 @@ public class MagnetGun : BaseItem
 			RaycastHit hit;
 			Vector3 fwd = owner.transform.TransformDirection(Vector3.forward);
 			Vector3 pos = new Vector3(0, 1, 0);
-=======
-            if (Physics.Raycast(owner.transform.position + pos, fwd, out hit, 20))
-            {
-                IMagnetic mag = hit.collider.gameObject.GetComponentInChildren<IMagnetic>();
->>>>>>> 5c874fbcd44fe2e8a0e88c9c8658823f3cc96609
 
-                if (mag != null)
-                {
-                    Vector3 cur = hit.collider.transform.position;
-                    if (Vector3.Distance(cur, owner.transform.position) > 3)
-                    {
-                        //If we have hit the mega thrum, trigger special logic
-                        if (hit.collider.gameObject.name == "ThrumTitan")
-                        {
-                            print("Found thrum titan");
-                            Titan titan = hit.collider.gameObject.GetComponent<Titan>();
-                            titan.pullThrum();
-                            return;
-                        }
-
-<<<<<<< HEAD
 			if(Physics.Raycast(owner.transform.position + pos, fwd, out hit, 20))
 			{
 
@@ -97,7 +50,7 @@ public class MagnetGun : BaseItem
 					Vector3 cur = hit.collider.transform.position;
 					if(Vector3.Distance(cur, owner.transform.position) > 3)
 					{
-=======
+
                         if (!mag.isHeavierThanPlayer())
                         {
                             hit.collider.transform.position =
@@ -128,16 +81,7 @@ public class MagnetGun : BaseItem
             particleSys.enableEmission = false;
         }
     }
->>>>>>> 5c874fbcd44fe2e8a0e88c9c8658823f3cc96609
 
-    private bool isParentMagnetic(GameObject g)
-    {
-        if (g.GetComponent<IMagnetic>() != null)
-        {
-
-        }
-
-<<<<<<< HEAD
 	//Used during raycast hit to determine if we have hit a child of a magnetic object
 	private Titan isParentTitan(GameObject g)
 	{
@@ -153,8 +97,4 @@ public class MagnetGun : BaseItem
 	{
 		return "Magnet Gun";
 	}
-=======
-        return false;
-    }
->>>>>>> 5c874fbcd44fe2e8a0e88c9c8658823f3cc96609
 }
