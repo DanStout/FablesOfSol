1ebf5c492546944a098c0dc09db1b0d494d6be03
Matt <burkem.1993@gmail.com>
2015-12-08 21:17:45 -0400

    End state scientist added

---
+++ Assets/Scripts/NPCs/FloeFinalScientistTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloeFinalScientistTrigger : ScientistTrigger {
+
+	public override void Operate()
+	{
+		dialog.ActiveNpcName = DisplayName;
+		dialog.AddLine("You've saved man kind!");
+
+		didOperate = true;
+	}
+}
