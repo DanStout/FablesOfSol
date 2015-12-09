2e3724988b550d090e544c84eacb487b41a0c186
DandiAndy <a9kenny@gmail.com>
2015-12-09 12:04:47 -0400

    Game is now playable, start to end.

--- Assets/Scripts/NPCs/ScientistTrigger.cs
+++ Assets/Scripts/NPCs/ScientistTrigger.cs
@@ -15,12 +15,23 @@ public class ScientistTrigger : BaseOperable
 
     public override void Operate()
     {
-        dialog.ActiveNpcName = DisplayName;
-        dialog.AddLine("Thank god you're here! I was beginning to worry I would have to go to Floe and Cronon to get those samples myself!");
-        dialog.AddLine("Oh. You've already got a hammer.. Looks like I won't be very useful after all..");
-        dialog.AddLine("Unfortunately the way to the Cronon teleporter was blocked when some rocks fell from the ceiling. You look like you might be strong enough to break them with that hammer, though.");
-        dialog.AddLine("Good luck!");
-        didOperate = true;
+		if(GameObject.FindGameObjectWithTag("ItemHolder").GetComponent<Hammer>() != null){
+	        dialog.ActiveNpcName = DisplayName;
+	        dialog.AddLine("Thank god you're here! I was beginning to worry I would have to go to Floe and Cronon to get those samples myself!");
+			dialog.AddLine("My research indicates that we need DNA from powerful creatures in Sol system to restore the colonies DNA back to normal.");
+	        dialog.AddLine("Oh. You've already got a hammer.. Looks like I won't be very useful after all..");
+	        dialog.AddLine("Now go break those rocks with that hammer!");
+	        dialog.AddLine("Good luck!");
+	        didOperate = true;
+		}else{
+			dialog.ActiveNpcName = DisplayName;
+			dialog.AddLine("Thank god you're here! I was beginning to worry I would have to go to Floe and Cronon to get those samples myself!");
+			dialog.AddLine("My research indicates that we need DNA from powerful creatures in Sol system to restore the colonies DNA back to normal.");
+			dialog.AddLine("You'll need a weapon though. If you return to the surface you can find a large hammer hidden behind the largest Boulder to the left.");
+			dialog.AddLine("Unfortunately the way to the Cronon teleporter was blocked when some rocks fell from the ceiling. You look like you might be strong enough to break them with that hammer, though.");
+			dialog.AddLine("Good luck!");
+			didOperate = true;
+		}
     }
 
     protected override void ReactLeftRange()
